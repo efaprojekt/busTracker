8c7dceb57df69f959625128e027d0d322499054d
Efa F <87249419+efaprojekt@users.noreply.github.com>
2021-09-26 10:49:39 -0700

    Add files via upload

---
+++ mapanimation.js
@@ -0,0 +1,49 @@
+// This array contains the coordinates for all bus stops between MIT and Harvard
+const busStops = [
+    [-71.093729, 42.359244],
+    [-71.094915, 42.360175],
+    [-71.0958, 42.360698],
+    [-71.099558, 42.362953],
+    [-71.103476, 42.365248],
+    [-71.106067, 42.366806],
+    [-71.108717, 42.368355],
+    [-71.110799, 42.369192],
+    [-71.113095, 42.370218],
+    [-71.115476, 42.372085],
+    [-71.117585, 42.373016],
+    [-71.118625, 42.374863],
+  ];
+  
+  mapboxgl.accessToken = 'pk.eyJ1IjoidGhhY2FycmEiLCJhIjoiY2t0ZHU4OWU2Mmo4ZTJvbGFjcjh3ZGZpdiJ9.rv6rsioLKQnroq7_x2dCwA';
+  
+  // This is the map instance
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.104081, 42.365554],
+    zoom: 14,
+  });
+  
+  // Map marker at first coordinates
+  let marker = new mapboxgl.Marker()
+  .setLngLat([-71.093729, 42.359244])
+  .addTo(map);
+  // index of the current bus stop
+  let counter = 0;
+  function move() {
+    // marker move every 1000ms. 
+    setTimeout(() => {
+      if (counter >= busStops.length) return;
+      marker.setLngLat(busStops[counter]);
+      counter++;
+      move();
+    }, 1000);
+    }
+    
+  }
+  
+  
+  if (typeof module !== 'undefined') {
+    module.exports = { move, counter, marker, busStops };
+  }
+  
